555bbad5f4f920a1215a686c0f2ee48089248327
lao <laorenfei@189.cn>
2017-06-16 17:12:16 +0800

    add re to the bug "csrf"

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from superlist.lists.views import home_page
 from django.template.loader import render_to_string
+import re
 class HomePageTest(TestCase):
      def test_root_url_resolves_to_home_page_view(self):
          found = resolve('/') # ➋
@@ -11,8 +12,13 @@ class HomePageTest(TestCase):
      def test_home_page_returns_corrent_html(self):
          request=HttpRequest()
          response=home_page(request)
-         expected_html=render_to_string('home.html')
-         self.assertEqual(response.content.decode(), expected_html)
+         expected_html = render_to_string('home.html',request=request)
+         print('response.content.decode()\n', response.content.decode())
+         print('expected_html\n', expected_html)
+         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+         observed_html = re.sub(csrf_regex, '', response.content.decode())
+         expected_html = re.sub(csrf_regex, '', expected_html)
+         self.assertEqual(observed_html, expected_html)
 
      def test_home_page_can_save_post_request(self):
          request=HttpRequest()
@@ -23,6 +29,8 @@ class HomePageTest(TestCase):
          self.assertIn('A new list item',response.content.decode())
          excected_html=render_to_string('home.html',
             {'new_item_text':'A new list item'})
-         self.assertEqual(response.content.decode(),excected_html)
+         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+         observed_html = re.sub(csrf_regex, '', response.content.decode())
+         self.assertEqual(observed_html,excected_html)
 
 
