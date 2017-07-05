0a6b4d70ab8d37ba323c2064864e7a9522442508
lao <laorenfei@189.cn>
2017-07-05 15:20:42 +0800

    makegrations

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from superlist.lists.views import home_page
 from django.template.loader import render_to_string
+from lists.models import Item
 import re
 class HomePageTest(TestCase):
      def test_root_url_resolves_to_home_page_view(self):
@@ -33,4 +34,17 @@ class HomePageTest(TestCase):
          observed_html = re.sub(csrf_regex, '', response.content.decode())
          self.assertEqual(observed_html,excected_html)
 
-
+class ItemModelTest(TestCase):
+     def test_saving_and_retrieving_items(self):
+         first_item = Item()
+         first_item.text = 'The first (ever) list item'
+         first_item.save()
+         second_item = Item()
+         second_item.text = 'Item the second'
+         second_item.save()
+         saved_items = Item.objects.all()
+         self.assertEqual(saved_items.count(), 2)
+         first_saved_item = saved_items[0]
+         second_saved_item = saved_items[1]
+         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+         self.assertEqual(second_saved_item.text, 'Item the second')
