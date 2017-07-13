18bba38136d5114ee228eb1a8f1ca95ade94c66c
lao <laorenfei@189.cn>
2017-07-13 14:23:36 +0800

    delete pyc

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -14,8 +14,6 @@ class HomePageTest(TestCase):
          request=HttpRequest()
          response=home_page(request)
          expected_html = render_to_string('home.html',request=request)
-     #    print('response.content.decode()\n', response.content.decode())
-     #    print('expected_html\n', expected_html)
          csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
          observed_html = re.sub(csrf_regex, '', response.content.decode())
          expected_html = re.sub(csrf_regex, '', expected_html)
@@ -25,24 +23,26 @@ class HomePageTest(TestCase):
          request=HttpRequest()
          request.method='POST'
          request.POST['item_text']=('A new list item')
+
          response=home_page(request)
-         print(response)
+
          self.assertEqual(Item.objects.count(), 1)
          new_item = Item.objects.first()
          self.assertEqual(new_item.text, 'A new list item')
+     def test_home_page_redirects_after_POST(self):
+         request=HttpRequest()
+         request.method='POST'
+         request.POST['item_text']='A new list item'
+         response=home_page(request)
          self.assertEqual(response.status_code,302)
          self.assertEqual(response['location'],'/')
-         #self.assertIn(new_item.text,response.content.decode())
-         #excected_html=render_to_string('home.html',
-           # {'new_item_text':'A new list item'})
-         #csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
-         #observed_html = re.sub(csrf_regex, '', response.content.decode())
-         #self.assertEqual(observed_html,excected_html)
 
      def test_home_page_only_saves_items_when_necessary(self):
          request=HttpRequest()
          home_page(request)
          self.assertEqual(Item.objects.count(),0)
+
+     def test_home_page
 
 class ItemModelTest(TestCase):
      def test_saving_and_retrieving_items(self):
