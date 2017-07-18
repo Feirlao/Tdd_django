a51bb9653bf7de1c96cd5adb07eae6dd339d1f57
lao <laorenfei@189.cn>
2017-07-18 13:25:31 +0800

    Redirect after POST, and show all items in template

--- superlist/funtional_tests/tests.py
+++ superlist/funtional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class newvistortest(unittest.TestCase):
+class newvistortest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -13,38 +13,13 @@ class newvistortest(unittest.TestCase):
         self.browser.implicitly_wait(5)
         self.browser.quit()
 
-    def check_for_row_in_list_table(self,row_text):
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        stan=[row.text for row in rows]
-        print(stan)
-        self.assertIn(row_text,stan)
-
-
     def test_can_start_a_list_and_retrieve(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('1:Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys('Buy peacock feathers')
 
+        inputbox.send_keys(Keys.ENTER)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
-        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
-
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2:Use peacock feathers to make a fly',
-            [row.text for row in rows]
-        )
-        self.fail('finish the test')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
