f6c9ab56029cc9a23b977c82323b8d8bbf77ad65
lao <laorenfei@189.cn>
2017-07-18 13:52:07 +0800

    1:

--- superlist/funtional_tests/tests.py
+++ superlist/funtional_tests/tests.py
@@ -13,13 +13,15 @@ class newvistortest(LiveServerTestCase):
         self.browser.implicitly_wait(5)
         self.browser.quit()
 
+
     def test_can_start_a_list_and_retrieve(self):
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
-
         inputbox.send_keys(Keys.ENTER)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+
+        self.fail('finish the test')
