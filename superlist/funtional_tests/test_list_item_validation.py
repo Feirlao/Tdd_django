a7dc7149308823dd6d1e73120a749c8c9dbfed1b
lao <laorenfei@189.cn>
2017-09-28 17:36:04 +0800

    add models test

--- superlist/funtional_tests/test_list_item_validation.py
+++ superlist/funtional_tests/test_list_item_validation.py
@@ -20,3 +20,12 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_inpt_box().send_keys('Make tea\n')
         self.check_for_row_in_list_table('1: Buy milk')
         self.check_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        self.browser.get(self.server_url)
+        self.get_item_inpt_box().send_keys('buy wellies\n')
+        self.check_for_row_in_list_table('1:buy wellies')
+        self.get_item_inpt_box().send_keys('buy wellies\n')
+        self.check_for_row_in_list_table('1:buy wellies')
+        error=self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text,'you have already got this in your list')
